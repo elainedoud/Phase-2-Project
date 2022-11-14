0cda8c00fae311869a2b7d795b4368e0f1863a1f
Cesar Hernandez <chema27281@gmail.com>
2022-11-14 10:15:30 -0500

    added Components

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,26 @@
+import NavBar from "./NavBar"
+
+
+
+
+function Header(){
+
+
+
+
+
+
+
+
+    return (
+
+
+        <>
+        <NavBar />
+        
+        </>
+    )
+}
+
+
+export default Header
---
+++ my-app/src/components/ThemeParkPage.js
@@ -0,0 +1,20 @@
+import ThemeParkCards from "./ThemeParkCards"
+
+
+
+function ThemeParkPage(){
+
+
+
+
+
+    return(
+        <>
+        <ThemeParkCards />
+        
+        </>
+    )
+}
+
+
+export default ThemeParkPage
