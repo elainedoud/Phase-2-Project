c8420febc8cdc3ba4c3a2e7a0655743b39864497
Elaine <elainedoud@gmail.com>
2022-11-18 10:19:35 -0500

    Content to About section

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -2,7 +2,9 @@ import React from "react"
 function About(){
 return(
         <p>
-            I AM A PIECE OF TEXT
+           Use this fun, helpful resource to view theme parks in the U.S.
+           and read reviews.  This website is kept up-to-date by people like
+           you.  Add your own review or add another amusement park!
         </p>
     )
 }
