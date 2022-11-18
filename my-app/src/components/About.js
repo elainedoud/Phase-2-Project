67d7682576d423afcb71daa5a25bda1c02615ef7
Elaine <elainedoud@gmail.com>
2022-11-18 10:36:07 -0500

    Changed font color of About section

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,7 +1,7 @@
 import React from "react"
 function About(){
 return(
-        <p>
+        <p className="about">
            Use this fun, helpful resource to view theme parks in the U.S.
            and read reviews.  This website is kept up-to-date by people like
            you.  Add your own review or add another amusement park!
