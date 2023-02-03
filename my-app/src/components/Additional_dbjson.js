588ecf6e3a8fc76c4ba98455a09806f1b75359a2
Elaine <elainedoud@gmail.com>
2023-02-03 11:59:02 -0500

    Made Theme Parks the main db:json

---
+++ my-app/src/components/Additional_dbjson.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function data() {
+
+{
+    "posts": [
+      {
+        "id": 1,
+        "title": "json-server",
+        "author": "typicode"
+      }
+    ],
+    "comments": [
+      {
+        "id": 1,
+        "body": "some comment",
+        "postId": 1
+      }
+    ],
+    "profile": {
+      "name": "typicode"
+    }
+  }
+
+  return (
+    <div></div>
+  )
+
+}
